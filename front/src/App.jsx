8ce6b281b7df4c6841cf678724897990b9874cd2
Hussien Mahmoud <114174179+Hussien0Mahmoud@users.noreply.github.com>
2025-04-16 14:31:23 +0200

    add home page and style folder and edit routes in app page

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,14 +1,21 @@
-import { Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css'
 import SharedLayout from './sharedlayout/SharedLayout.jsx'
+import { Home } from "./pages/Home.jsx";
 
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}></Route>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route  index element={<Home />} />
+          <Route path="home"  element={<Home />} />
+        </Route>
       </Routes>
+    </BrowserRouter>
+    
   )
 }
 
---
+++ front/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { FaDownload, FaUsers, FaStar, FaArrowRight } from 'react-icons/fa';
+import Hero from '../components/Home/Hero';
+// import BrowseCategories from '../components/Home/BrowseCategories';
+
+export function Home() {
+  return (
+    <>
+      {/* Hero Section */}
+      <Hero />
+
+      
+      
+
+    </>
+  );
+}

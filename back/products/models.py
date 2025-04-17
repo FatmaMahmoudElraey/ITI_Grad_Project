b178f834035183a23e75bc14dac93c32bc66f1e2
hosam <semryhosam@gmail.com>
2025-04-17 19:54:02 +0200

    configured products models

--- back/products/models.py
+++ back/products/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products')
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='products')
+    tags = models.ManyToManyField(Tag, blank=True)
+    file = models.FileField(upload_to='products/files/')
+    preview_image = models.ImageField(upload_to='products/previews/images/', null=True, blank=True)
+    preview_video = models.FileField(upload_to='products/previews/videos/', null=True, blank=True)
+    live_demo_url = models.URLField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_in_subscription = models.BooleanField(default=True)
+    is_approved = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class ProductReview(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('product', 'user')
+
+
+class ProductFlag(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='flags')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    reason = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('product', 'user')

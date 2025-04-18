678b717c955a1ea16beaafeec36a0fc409211c67
hosam <semryhosam@gmail.com>
2025-04-19 00:20:13 +0200

    Added validator to keep rating between 0-5

--- back/products/models.py
+++ back/products/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 User = settings.AUTH_USER_MODEL
 
@@ -52,7 +54,7 @@ class Product(models.Model):
 class ProductReview(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.PositiveSmallIntegerField()
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 

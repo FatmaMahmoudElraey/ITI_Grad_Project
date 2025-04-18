52b38ba86303f2aef06434d29129d73703f975c5
mohmdloai <20812016100937@eng.zu.edu.eg>
2025-04-18 22:27:42 +0200

    Create UserAccount model

--- back/accounts/models.py
+++ back/accounts/models.py
@@ -1,1 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+
+
+class UserAccount(AbstractUser,PermissionsMixin):
+    email = models.EmailField(max_length=255,unique=True)
+
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    username = None
+    USERNAME_FIELD = 'email' # Defualt required login field.
+
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.email

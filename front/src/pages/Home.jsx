5a51cb80a1df6ba818e9d54d52b2ef50ab95ad8c
Hussien Mahmoud <114174179+Hussien0Mahmoud@users.noreply.github.com>
2025-04-22 13:38:31 +0200

    Finished Home page

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
-import { FaDownload, FaUsers, FaStar, FaArrowRight } from 'react-icons/fa';
+
 import Hero from '../components/Home/Hero';
-import BrowseCategories from '../components/Home/BrowseCategories';
 import StateSection from '../components/Home/StateSection';
-// import TemplateCard from '../components/Home/TemplateCard';
 import Testimonials from '../components/Home/Testimonials';
+import FeaturedTemplates from '../components/Home/FeaturedTemplates';
+import CategoriesSection from '../components/Home/CategoriesSection';
 
 export function Home() {
   return (
     <>
-      {/* Hero Section */}
       <Hero />
-      <BrowseCategories />
       <StateSection />
+      <CategoriesSection />
+      <FeaturedTemplates />
       <Testimonials />
-      {/* <TemplateCard /> */}
       
-      
-
     </>
   );
 }

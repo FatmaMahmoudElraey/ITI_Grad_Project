eed7ee10b27b8451aab2755e564a8ae5c9516822
mohmdloai <20812016100937@eng.zu.edu.eg>
2025-04-23 11:52:12 +0200

    Mount auth on single api

--- back/webify/urls.py
+++ back/webify/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/accounts/', include('accounts.urls')),
-    path('api-auth/', include('rest_framework.urls')),
+    path('api/auth/', include('authsys.urls')),
+    path('api/auth/', include('accounts.urls')),
+    #NOTE: ROUTER TO CATCH ALL OTHER NOT HANDLED URLS, MUST PUT IN BOTTOM.
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]

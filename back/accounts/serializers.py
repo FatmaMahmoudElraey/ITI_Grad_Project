5b155620f0edbb164b9356e63b11b5ddb0f302e7
mohmdloai <20812016100937@eng.zu.edu.eg>
2025-04-23 11:48:51 +0200

    Set model name to User

--- back/accounts/serializers.py
+++ back/accounts/serializers.py
@@ -1,17 +1,23 @@
 from rest_framework import serializers
 from djoser.serializers import UserCreateSerializer
+from .models import UserAccount
 from django.contrib.auth import get_user_model
-user = get_user_model()
+User = get_user_model()
 
 
 class UserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
-        model = user
+        model = User
         fields = ('id', 'email', 'name', 'password')#TODO: CHANGE to fname, lname ... etc.
 
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = user
-        fields = ("id", "email", "name")  
+        model = User
+        fields = ("id", "email", "name")
+
+class CustomUserSerializer(UserSerializer):
+    class Meta(UserSerializer.Meta):
+        model = UserAccount
+        fields = ('id', 'email', 'name')

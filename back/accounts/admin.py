d6fe4c73d2f753f93b3fda784347f00b2a27f0b6
mohmdloai <20812016100937@eng.zu.edu.eg>
2025-04-23 15:44:40 +0200

    fixed authentication error

--- back/accounts/admin.py
+++ back/accounts/admin.py
@@ -1,2 +1,10 @@
 from django.contrib import admin
+from .models import UserAccount
 
+@admin.register(UserAccount)
+class UserAccountAdmin(admin.ModelAdmin):  # Changed class name to UserAccountAdmin
+    list_display = ('id', 'email', 'name', 'is_active', 'is_staff')  # Removed username since it's None
+    search_fields = ('email', 'name')  # Removed username since it's None
+    list_filter = ('is_active', 'is_staff')
+    ordering = ('-id',)
+    list_per_page = 20
--- back/webify/urls.py
+++ back/webify/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('api/auth/', include('authsys.urls')),
     path('api/auth/', include('accounts.urls')),
     #NOTE: ROUTER TO CATCH ALL OTHER NOT HANDLED URLS, MUST PUT IN BOTTOM.
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+    # re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
     path('api/', include('products.urls')),
     path('api/', include('orders.urls')),
 ]

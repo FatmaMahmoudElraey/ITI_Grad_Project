55e609de3a1b301c8a03e7438089af1ae98711fc
hosam <semryhosam@gmail.com>
2025-04-26 00:41:19 +0300

    Added corsheaders

--- back/webify/settings.py
+++ back/webify/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework_simplejwt.token_blacklist',
     'djoser',
+    'corsheaders',
     'accounts.apps.AccountsConfig',
     'authsys.apps.AuthsysConfig',
     'products',
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -92,6 +94,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'webify.wsgi.application'
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://localhost:8000",
+]
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases

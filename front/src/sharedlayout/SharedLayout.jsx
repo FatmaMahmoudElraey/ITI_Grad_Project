30523de370315328399a07cc5f4ad0494375203c
FatmaMahmoudElraey <fatmamahmoudelraey.22@gmail.com>
2025-04-27 15:13:39 +0300

    changed shared layout

--- front/src/sharedlayout/SharedLayout.jsx
+++ front/src/sharedlayout/SharedLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Navbar";
+// import FloatingChatbot from "../components/Chatbot/FloatingChatbot";
 
 
 export default function SharedLayout() {
@@ -8,7 +9,9 @@ export default function SharedLayout() {
         <>
             <Header />
             <Outlet />
+            {/* <FloatingChatbot /> */}
             <Footer />
         </>
     )
 }
+// 

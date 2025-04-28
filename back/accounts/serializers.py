9d2c83d1c1226800865caa138c27aa1ff3230c01
hosam <semryhosam@gmail.com>
2025-04-29 00:01:18 +0300

    Added endpoints for chat

--- back/accounts/serializers.py
+++ back/accounts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from djoser.serializers import UserCreateSerializer
-from .models import UserAccount, UserProfile, Favorite
+from .models import UserAccount, UserProfile, Favorite, ChatMessage
 from django.contrib.auth import get_user_model
 from orders.serializers import OrderSerializer
 from products.serializers import ProductSerializer
@@ -45,4 +45,32 @@ class UserProfileSerializer(serializers.ModelSerializer):
         orders = Order.objects.filter(user=obj.user)
         return OrderSerializer(orders, many=True).data
     
-
+class ChatMessageSerializer(serializers.ModelSerializer):
+    sender_name = serializers.SerializerMethodField()
+    receiver_name = serializers.SerializerMethodField()
+    sender_picture = serializers.SerializerMethodField()
+    receiver_picture = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = ChatMessage
+        fields = ('id', 'user', 'sender', 'reciever', 'message', 'is_read', 'date', 
+                 'sender_name', 'receiver_name', 'sender_picture', 'receiver_picture')
+        read_only_fields = ('id', 'date', 'user')
+        
+    def get_sender_name(self, obj):
+        return obj.sender.name if obj.sender else None
+        
+    def get_receiver_name(self, obj):
+        return obj.reciever.name if obj.reciever else None
+        
+    def get_sender_picture(self, obj):
+        try:
+            return obj.sender_profile.picture.url if obj.sender_profile.picture else None
+        except:
+            return None
+            
+    def get_receiver_picture(self, obj):
+        try:
+            return obj.reciever_profile.picture.url if obj.reciever_profile.picture else None
+        except:
+            return None
--- back/accounts/urls.py
+++ back/accounts/urls.py
@@ -1,12 +1,15 @@
-from django.urls import path
-from .views import UserDetailView, UserProfileDetailView
+from django.urls import path, include
+from .views import UserDetailView, UserProfileDetailView, UserListView, ChatMessageViewSet
 from rest_framework.routers import DefaultRouter
 from .views import FavoriteViewSet
 
 router = DefaultRouter()
 router.register(r'favorites', FavoriteViewSet, basename='favorite')
+router.register(r'chat-messages', ChatMessageViewSet, basename='chat-messages')
 
 urlpatterns = [
     path('users/me/', UserDetailView.as_view(), name='user-detail'),
     path('users/me/profile/', UserProfileDetailView.as_view(), name='user-profile-detail'),
-]+ router.urls
+    path('customers/', UserListView.as_view(), name='user-list'),
+    path('', include(router.urls)),
+]

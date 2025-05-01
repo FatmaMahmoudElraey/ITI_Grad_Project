b499dfdee482ea881bebb94b9c06beead1b05d01
hosam <semryhosam@gmail.com>
2025-05-01 14:49:40 +0300

    Added is_featured field to products

--- back/products/models.py
+++ back/products/models.py
@@ -39,6 +39,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     is_in_subscription = models.BooleanField(default=True)
     is_approved = models.BooleanField(default=False)
+    is_featured = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):

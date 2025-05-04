ddbc65cfc2f56fff619703560853e455bb4127b2
hosam <semryhosam@gmail.com>
2025-05-04 22:24:28 +0300

    Imported jquery in main.jsx

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -2,6 +2,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+window.jQuery = window.$ = $;
 import 'bootstrap/dist/js/bootstrap.bundle';
 import store from './store/index.js';
 import { Provider } from 'react-redux';

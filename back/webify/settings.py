fca0dbd19342c166bc5d6b2a4b0f1207615ef12f
mohmdloai <mhmd.00.eng@gmail.com>
2025-10-04 23:40:53 +0300

    Update: settings with staticfiles dir root

--- back/webify/settings.py
+++ back/webify/settings.py
@@ -234,6 +234,13 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Optional but recommended
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 

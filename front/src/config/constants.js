66297b40d7d6e5c95285a632cd14ff3f55e3b9a8
mohmdloai <mhmd.00.eng@gmail.com>
2025-10-08 00:13:38 +0300

    Fix baseUrls
    VITE_API_BASE_URL
    
    VITE_APP_FRONTEND_URL

--- front/src/config/constants.js
+++ front/src/config/constants.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:8000";
-export const FRONTEND_URL = "http://localhost:5173";
+export const BASE_URL = `${import.meta.env.VITE_API_BASE_URL}`;
+export const FRONTEND_URL = `${import.meta.env.VITE_APP_FRONTEND_URL}`;
 export const GOOGLE_OAUTH2_CLIENT_ID =
   "849946536066-ooju2jjmk75vcoksb7hipif8bimog9kk.apps.googleusercontent.com";
 

87c35cf373f680b9e2e0c0ad972e62becd5689e6
hosam <semryhosam@gmail.com>
2025-10-08 22:57:56 +0300

    Enhance: Change page size to 6

--- back/products/pagination.py
+++ back/products/pagination.py
@@ -4,7 +4,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 class ProductPagination(PageNumberPagination):
-    page_size = 5
+    page_size = 6
     page_size_query_param = "page_size"
     max_page_size = 100
 

afc7575905547fc723ae23389371af323cd2c9f6
hosam <semryhosam@gmail.com>
2025-10-11 18:23:35 +0300

    Enhance: Modify latest products component to be a slider for better user experience

--- front/src/components/LatestProducts.jsx
+++ front/src/components/LatestProducts.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchLatestProducts } from '../store/slices/productsSlice';
-import TemplateCard from './TemplateCard'; 
+import TemplateCard from './TemplateCard';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 import { Row, Col, Spinner, Alert } from 'react-bootstrap';
 
 const LatestProducts = () => {
@@ -14,23 +16,36 @@ const LatestProducts = () => {
 
   return (
     <div style={{ marginTop: '2rem', marginBottom: '2rem' }}>
-      <h2 className="mb-3"> 
+      <h2 className="my-4 text-center text-light p-2" style={{backgroundColor: '#660ff1'}}> 
         Latest Products
       </h2>
       {loading && <Spinner animation="border" role="status"><span className="visually-hidden">Loading...</span></Spinner>} 
       {error && <Alert variant="danger">{error}</Alert>} 
       {!loading && !error && latestItems.length > 0 && (
-        <Row xs={1} sm={2} md={3} lg={4} className="g-4"> 
-          {latestItems.map((product) => (
-            <Col key={product.id}> 
-              <TemplateCard 
-                {...product}
-                reviews={product.reviews || []}
-                tags={product.tags || []}
-              />
-            </Col>
-          ))}
-        </Row>
+        <div>
+          <Swiper
+            spaceBetween={16}
+            slidesPerView={1.2}
+            breakpoints={{
+              480: { slidesPerView: 1.2 },
+              600: { slidesPerView: 2 },
+              768: { slidesPerView: 2 },
+              992: { slidesPerView: 4 },
+            }}
+          >
+            {latestItems.map((product) => (
+              <SwiperSlide key={product.id}>
+                <div style={{height: '100%'}}>
+                  <TemplateCard
+                    {...product}
+                    reviews={product.reviews || []}
+                    tags={product.tags || []}
+                  />
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       )}
       {!loading && !error && latestItems.length === 0 && (
         <p>No latest products found.</p> 

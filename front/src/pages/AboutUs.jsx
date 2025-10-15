fba504f7642c9bdea32d0ae5e5ed28c7a5ce95fc
hosam <semryhosam@gmail.com>
2025-10-16 02:42:57 +0300

    Remve unused imports

--- front/src/pages/AboutUs.jsx
+++ front/src/pages/AboutUs.jsx
@@ -1,29 +1,3 @@
-import React from 'react'
-import { Link } from "react-router-dom";
-import { Container, Row, Col, Form, Button, Card } from "react-bootstrap";
-import { 
-  FaPhone, 
-  FaEnvelope, 
-  FaMapMarkerAlt, 
-  FaClock, 
-  FaFacebook, 
-  FaTwitter, 
-  FaLinkedin, 
-  FaInstagram, 
-  FaStar, 
-  FaUsers, 
-  FaShoppingCart, 
-  FaShieldAlt,
-  FaShippingFast,
-  FaTags,
-  FaHeadset
-} from "react-icons/fa";
-
-import img1 from '../assets/images/about/1.jpg.png'
-import img2 from '../assets/images/about/2.jpg.png'
-import img3 from'../assets/images/about/3.jpg.png'
-import about1 from'../assets/images/about/about-1.webp'
-
 import '../styles/about/about.css';
 import AboutHero from './../components/AboutUs/AboutHero';
 import AboutValues from '../components/AboutUs/AboutValues';

0daa5a7effeaf64881767c90a45958fbc4f2545f
mohmdloai <mhmd.00.eng@gmail.com>
2025-10-20 03:54:55 +0300

    FIX: Import ENDPOINTS
    issue: using ENDPOINTS without importing it

--- front/src/pages/UserProfile.jsx
+++ front/src/pages/UserProfile.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
-import {
+import { ENDPOINTS } from "../api/constants";
+=import {
   fetchUserProfile,
   deleteUserAccount,
   updateUserProfile,

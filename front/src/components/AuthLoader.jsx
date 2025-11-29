fb5e060fb9d659eade3929ac509abf298a276f59
hosam <semryhosam@gmail.com>
2025-11-29 23:16:48 +0200

    Refactor AuthLoader to robustly refresh and load user on app start

--- front/src/components/AuthLoader.jsx
+++ front/src/components/AuthLoader.jsx
@@ -1,21 +1,16 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { loadUser } from '../store/slices/authSlice';
+import { loadUser, refreshToken } from '../store/slices/authSlice';
 
-/**
- * Component that loads user data on application startup
- * This ensures that user role information is available for protected routes
- */
 const AuthLoader = ({ children }) => {
   const dispatch = useDispatch();
   const { isAuthenticated, user } = useSelector((state) => state.auth);
-  
+
   useEffect(() => {
-    // Only try to load user data if we have a token but no user data yet
-    if (isAuthenticated && !user) {
+    dispatch(refreshToken()).finally(() => {
       dispatch(loadUser());
-    }
-  }, [dispatch, isAuthenticated, user]);
+    });
+  }, [dispatch]);
 
   return children;
 };

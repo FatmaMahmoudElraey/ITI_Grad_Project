c4dc810b8812cd195eb6155c15f4a0968664ba6a
hosam <semryhosam@gmail.com>
2025-12-08 15:22:17 +0200

    Refactor AuthLoader to include path skipping logic for authentication

--- front/src/components/AuthLoader.jsx
+++ front/src/components/AuthLoader.jsx
@@ -1,16 +1,30 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 import { loadUser, refreshToken } from '../store/slices/authSlice';
 
 const AuthLoader = ({ children }) => {
   const dispatch = useDispatch();
   const { isAuthenticated, user } = useSelector((state) => state.auth);
+  const location = useLocation();
 
   useEffect(() => {
+    const pathname = location.pathname || '';
+    const skipPaths = [
+      '/login',
+      '/register',
+      '/reset-password',
+      '/reset-password-confirm',
+      '/activate',
+    ];
+
+    const shouldSkip = skipPaths.some((p) => pathname.startsWith(p));
+    if (shouldSkip) return;
+
     dispatch(refreshToken()).finally(() => {
       dispatch(loadUser());
     });
-  }, [dispatch]);
+  }, [dispatch, location.pathname]);
 
   return children;
 };

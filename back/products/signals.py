1b4610657044d85a030771a8d3bad3355fef06bc
hosam <semryhosam@gmail.com>
2025-12-09 12:50:55 +0200

    Specify cache keys to be cleared in signals file

--- back/products/signals.py
+++ back/products/signals.py
@@ -7,4 +7,12 @@ from .models import Product, Category, Tag
 @receiver([post_save, post_delete], sender=Category)
 @receiver([post_save, post_delete], sender=Tag)
 def clear_cache_on_change(sender, **kwargs):
-    cache.clear()
+    cache_keys = [
+        'product_list',
+        'category_list',
+        'tag_list',
+        'latest_products',
+    ]
+    
+    for key in cache_keys:
+        cache.delete(key)
